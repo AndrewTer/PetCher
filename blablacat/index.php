691ef6fe3337f3f289dee6eccecf4d5798054d37
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-19 01:29:20 +0300

    Update index.php

--- blablacat/index.php
+++ blablacat/index.php
@@ -10,6 +10,26 @@ $email = $_SESSION['email'];
 $phone = $_SESSION['phone'];
 $address = $_SESSION['address'];
 $id = $_SESSION['id']; 
+
+$sort = $_GET["sort"];
+    switch($sort){
+        case 'all-orders':
+        $sort = " id DESC";
+        $sort_name = 'Все';
+        break;
+        case 'performed':
+        $sort = " confirmed = 'yes' DESC";
+        $sort_name = 'Выполненные';
+        break;
+        case 'current':
+        $sort = " confirmed = 'no' DESC";
+        $sort_name = 'Текущие';
+        break;
+        default:
+        $sort = " id DESC";
+        $sort_name = 'Все';
+        break;
+    }
 ?>
 
 <html>
@@ -17,6 +37,7 @@ $id = $_SESSION['id'];
     <link rel="stylesheet" href="css/main.css" type="text/css" />
     <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
     <script type="text/javascript" src="js/click-carousel.js"></script> 
+    <script type="text/javascript" src="js/script.js"></script> 
     <script type="text/javascript">
     $(function(){
         $(".containerr").clickCarousel({margin: 10});
@@ -34,6 +55,12 @@ $id = $_SESSION['id'];
             $row_new = mysql_fetch_array($result_new);
         }
         
+        $result_orders = mysql_query("SELECT orders.id AS order_id, orders.owner_id AS owner_id, pets.id AS pet_id, date_out, date_in, cost, pets.name AS pet_name, pets.kind as pet_kind, pets.breed AS pet_breed, pets.sex AS pet_sex, pets.weight AS pet_weight, pets.growth AS pet_growth, pets.photo AS avatar, orders.other_information AS about_order, pets.other_information AS about_pet FROM orders, pets WHERE (orders.owner_id = ".$id.") AND (orders.owner_id=pets.owner_id) AND (orders.pet_id=pets.id)");
+        if (mysql_num_rows($result_orders) > 0)
+        {
+            $row_orders = mysql_fetch_array($result_orders);
+        }
+        
         
     ?>
   
@@ -48,8 +75,56 @@ $id = $_SESSION['id'];
     ?>
         
         <div class="main-part-body">
-            <div class="favorites-part">
-                Избранное
+            <div class="orders-part">
+                <div id="block-title-and-sorting">
+                    <div class="block-title-and-sorting-left">
+                        <p class="title-section-main-body">Мои заказы</p>
+                    </div>
+                    <div class="block-title-and-sorting-right">
+                        <ul id="options-list">
+                            <li>Сортировать</li>
+                            <li><a id="select-links" href="#"><? echo $sort_name; ?></a>
+                            <ul id="list-links-sort">
+                                <a href="index.php?sort=all-orders"><li><strong>Все</strong></li></a>
+                                <a href="index.php?sort=current"><li><strong>Текущие</strong></li></a>
+                                <a href="index.php?sort=performed"><li><strong>Выполненные</strong></li></a>
+                            </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+                
+                <?
+                    if (mysql_num_rows($result_orders) == null)
+                    {
+                        echo '<hr /><p class="not-order">Вы пока не создали ни одного заказа</p>';
+                    }else
+                    {
+                        do{
+                            echo '
+                            <hr />
+                            <div class="current-order">
+                                <div class="left-part-order-list">
+                                    <div id="avatar-pet">
+                                        
+                                    </div>
+                                </div>
+                                <div class="right-part-order-list">
+                                    <p class="order-about">'.$row_orders["about_order"].'</p>
+                                    <p class="order-about">Даты: с '.$row_orders["date_out"].' до '.$row_orders["date_in"].'</p>
+                                    <p class="order-about">Животное: '.$row_orders["pet_kind"].' ('.$row_orders["pet_sex"].')</p>
+                                    <p class="order-about">Кличка: '.$row_orders["pet_name"].'</p>
+                                    <p class="order-about">Порода: '.$row_orders["pet_breed"].'</p>
+                                    <p class="order-about">Рост | Вес: '.$row_orders["pet_growth"].' м | '.$row_orders["pet_weight"].' кг</p>
+                                    <p class="order-cost">Цена: '.$row_orders["cost"].' руб</p>
+                                </div>
+                            </div>
+                            ';
+                
+                        }while ($row_orders = mysql_fetch_array($result_orders));
+                    }
+                ?>
             </div>
         </div>
     </div>
