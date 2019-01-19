46baed7c953b52ada9da8769574138f108e2fe65
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-19 20:00:18 +0300

    Add files via upload

---
+++ blablacat/404.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+    <title>404 Not Found | BlaBlaCat</title> 
+</head>
+
+<div class="grid-container">
+    <div class="parent">
+        <h1 class="not-found-title">Ошибка 404</h1>
+        <div class="not-found">
+            <div class="not-found-info">
+                <p style="font: 18px sans-serif; color: #383838;">К сожалению, запрашиваемая Вами страница не найдена..</p>
+                <p style="font: 17px sans-serif; color: #383838;">Почему?</p>
+                <ol style="font: 17px sans-serif; color: #383838;">
+                    <li>Ссылка, по которой Вы перешли, неверна.
+                    <li>Вы неправильно указали путь или название страницы.
+                    <li>Страница была удалена со времени Вашего последнего посещения.
+                </ol>
+                <p style="font: 18px sans-serif; color: #383838;">Для продолжения работы с сайтом Вы можете перейти на <a id="main" href="index.php">Главную страницу сайта.</a></p>
+            </div>
+            <div class="not-found-image">
+                <img src="images/404.png" width="auto" height="174" />
+            </div>            
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+</html>
