07bdb84cc37410902658067ed11a2bba02147d6c
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-19 19:59:15 +0300

    Update header.php

--- blablacat/includes/header.php
+++ blablacat/includes/header.php
@@ -4,8 +4,8 @@
         <a href=""><img id = "logos" src='images/logo.png' width="150" height="50" /></a>
         <nav>
             <a href="index.php?id<?echo $id;?>"><div class="menu"><? echo $username ?></div></a>
-            <a href=""><div class="menu">Настройки</div></a>
-            <a href=""><div class="menu">Помощь</div></a>
+            <a href="settings.php"><div class="menu">Настройки</div></a>
+            <a href="support.php"><div class="menu">Помощь</div></a>
             <a href="logout.php"><div class="menu">Выйти</div></a>
         </nav>
     </div>
