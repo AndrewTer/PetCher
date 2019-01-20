86fc5d02c889a5e6e2424c67182adb28f79c9af4
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-21 01:00:59 +0300

    Update search.php

--- blablacat/search.php
+++ blablacat/search.php
@@ -31,8 +31,39 @@ $username = $_SESSION['username'];
                 </div>
                 <div class="search-list">
                     <?
-                        $result_search = mysql_query("SELECT * FROM ");
-                    
+                        $result_search = mysql_query("SELECT * FROM orders WHERE (orders.owner_id != $id)  AND (orders.date_out>=curdate())");
+                        if (mysql_num_rows($result_search) == null)
+                        {
+                            echo '<hr /><p class="not-order">Заказов нет</p>';
+                        }else
+                        {
+                            $row_search = mysql_fetch_array($result_search);
+                            do{
+                                echo '
+                            <hr />
+                            <div class="current-order">
+                                    
+                                    <div class="left-part-order-list">
+                                        <div id="avatar-pet">';
+                                            
+                                                echo '<img class="image-avatar" src="images/nophoto.jpg" width="100%" />';
+                                            
+                                    echo '</div>
+                                    </div>
+                                    <div class="right-part-order-list">
+                                        <p class="order-about">'.$row_search["about_order"].'</p>
+                                        <p class="order-about">Даты: с '.$row_search["date_out"].' до '.$row_search["date_in"].'</p>
+                                        <p class="order-about">Животное: </p>
+                                        <p class="order-about">Кличка: </p>
+                                        <p class="order-about">Порода: </p>
+                                        <p class="order-about">Рост | Вес: </p>
+                                        <p class="order-cost">Цена: '.$row_search["cost"].' руб</p>
+                                    </div>
+                                    <div class="clear"></div>
+                            </div>
+                            ';
+                            }while ($row_search = mysql_fetch_array($result_search));
+                        }
                     ?>
                 </div>
             </div>
@@ -42,6 +73,7 @@ $username = $_SESSION['username'];
                         <p class="title-section-main-body">Параметры</p>
                     </div>
                 </div>
+                
             </div>
             <div class="clear"></div>
         </div>
