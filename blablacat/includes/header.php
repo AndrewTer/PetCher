229c441e8a489cd1bd8519a4aac069b7d714c2b8
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-20 09:09:36 +0300

    Update header.php

--- blablacat/includes/header.php
+++ blablacat/includes/header.php
@@ -1,3 +1,17 @@
+<?
+    $result_new = mysql_query("SELECT * FROM users WHERE full_name = '".$username."' AND password = '".$encrypted_password."'"); // AND email = '".$email."'");
+    if (mysql_num_rows($result_new) > 0)
+    {
+        $row_new = mysql_fetch_array($result_new);
+    }
+        
+    $result_requests = mysql_query("SELECT orders.owner_id AS owner_id, orders.pet_id AS pet_id, orders.date_out AS date_out, orders.date_in AS date_in, orders.cost AS cost, orders.other_information AS order_info, request.sitter_id AS sitter_id FROM orders, request WHERE (orders.id = request.order_id) AND (orders.owner_id = $id) AND (orders.kind = 'current')");
+    if (mysql_num_rows($result_requests) > 0)
+    {
+        $row_requests = mysql_fetch_array($result_requests);
+        $count_requests = mysql_num_rows($result_requests);
+    }
+?>
 <div class="header">
     <div class="contain clearfix">
 
