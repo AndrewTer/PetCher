12920e3562115ef95a397f5c8a05684333606415
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-22 00:02:23 +0300

    Update search.php

--- blablacat/search.php
+++ blablacat/search.php
@@ -31,7 +31,7 @@ $username = $_SESSION['username'];
                 </div>
                 <div class="search-list">
                     <?
-                        $result_search = mysql_query("SELECT * FROM orders WHERE (orders.owner_id != $id)  AND (orders.date_out>=curdate())");
+                        $result_search = mysql_query("SELECT orders.date_out AS date_out, orders.date_in AS date_in, orders.cost AS cost, orders.other_information AS about_order, pets.name AS pet_name, pets.kind AS pet_kind, pets.sex AS pet_sex, pets.breed AS pet_breed, pets.growth AS pet_growth, pets.weight AS pet_weight FROM orders, pets WHERE (orders.pet_id = pets.id) AND (orders.owner_id != $id)  AND (orders.date_out>=curdate()) GROUP BY orders.id");
                         if (mysql_num_rows($result_search) == null)
                         {
                             echo '<hr /><p class="not-order">Заказов нет</p>';
@@ -41,23 +41,20 @@ $username = $_SESSION['username'];
                             do{
                                 echo '
                             <hr />
-                            <div class="current-order">
-                                    
-                                    <div class="left-part-order-list">
-                                        <div id="avatar-pet">';
-                                            
-                                                echo '<img class="image-avatar" src="images/nophoto.jpg" width="100%" />';
-                                            
-                                    echo '</div>
+                            <div class="current-order-search">
+                                    <div class="left-part-order-search-list">
+                                        <div id="avatar-pet">
+                                            <img class="image-avatar" src="images/nophoto.jpg" width="100%" />
+                                        </div>
                                     </div>
-                                    <div class="right-part-order-list">
-                                        <p class="order-about">'.$row_search["about_order"].'</p>
-                                        <p class="order-about">Даты: с '.$row_search["date_out"].' до '.$row_search["date_in"].'</p>
-                                        <p class="order-about">Животное: </p>
-                                        <p class="order-about">Кличка: </p>
-                                        <p class="order-about">Порода: </p>
-                                        <p class="order-about">Рост | Вес: </p>
-                                        <p class="order-cost">Цена: '.$row_search["cost"].' руб</p>
+                                    <div class="right-part-order-search-list">
+                                        <p class="order-about-search">'.$row_search["about_order"].'</p>
+                                        <p class="order-about-search">Даты: с '.$row_search["date_out"].' до '.$row_search["date_in"].'</p>
+                                        <p class="order-about-search">Животное: '.$row_search["pet_kind"].' ('.$row_search["pet_sex"].')</p>
+                                        <p class="order-about-search">Кличка: '.$row_search["pet_name"].'</p>
+                                        <p class="order-about-search">Порода: '.$row_search["pet_breed"].'</p>
+                                        <p class="order-about-search">Рост | Вес: '.$row_search["pet_growth"].' м | '.$row_search["pet_weight"].' кг</p>
+                                        <p class="order-cost-search">Цена: '.$row_search["cost"].' руб</p>
                                     </div>
                                     <div class="clear"></div>
                             </div>
