f841c6cebdf867c6ec30e17ee26c94121a4bd111
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-23 21:02:27 +0300

    Update search.php

--- blablacat/search.php
+++ blablacat/search.php
@@ -77,9 +77,37 @@ $username = $_SESSION['username'];
     </div>
   </div>
   
-  <div class="footer">
-    BlaBlaCat © 2019
-  </div>
+  <script>
+    $(document).ready(function() {
+        $('body').append('<div class="button-up" style="display: none; margin-top:50px; opacity: 0.7;width: 100%;max-width:90px;height:100%;position: fixed;left: 0px;top: 0px;cursor: pointer;text-align: center;line-height: 100px;color: #45688E;">&#9650; Наверх</div>');
+        $ (window).scroll (function () {
+            if ($ (this).scrollTop () > 300) {
+            $ ('.button-up').fadeIn();
+            } else {
+            $ ('.button-up').fadeOut();
+            }
+        });
+        $('.button-up').click(function(){
+            $('body,html').animate({
+                scrollTop: 0
+            }, 100);
+            return false;
+        });
+        $('.button-up').hover(function() {
+            $(this).animate({
+                'opacity':'1',
+            }).css({'background-color':'#E1E7ED','color':'#45688E'});
+        }, function(){
+            $(this).animate({
+                'opacity':'0.7'
+            }).css({'background':'none','color':'#45688E'});;
+        });
+    });
+</script>
+
+<?php 
+    include("includes/footer.php");
+?> 
   
 </div>
 </html>
