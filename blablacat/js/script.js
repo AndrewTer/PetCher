1191e6b78cd286be3fc9a4a66e81da6bde6f83ae
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-24 04:08:11 +0300

    Update script.js

--- blablacat/js/script.js
+++ blablacat/js/script.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
     $('#select-links').click(function(){
         $("#list-links").slideToggle(200);
         $("#list-links-sort").slideToggle(200);    
-    });  
+    }); 
+    
+    $('.block-add-order').click(function(){
+        $('.block-add-order-info').slideToggle(300);     
+    }); 
 });
