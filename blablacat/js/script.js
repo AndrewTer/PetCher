7ae776b7a7e442922c1ec748c0838fdaae80ca85
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-01-29 20:01:36 +0300

    Update script.js

--- blablacat/js/script.js
+++ blablacat/js/script.js
@@ -54,4 +54,81 @@ $(document).ready(function(){
 			);
 	});
     
+    
+    /*Загрузка фото пользователя*/
+   	var dropZone = $('#upload-container');
+
+	$('#file-input').focus(function() {
+		$('label').addClass('focus');
+	})
+	.focusout(function() {
+		$('label').removeClass('focus');
+	});
+
+
+	dropZone.on('drag dragstart dragend dragover dragenter dragleave drop', function(){
+		return false;
+	});
+
+	dropZone.on('dragover dragenter', function() {
+		dropZone.addClass('dragover');
+	});
+
+	dropZone.on('dragleave', function(e) {
+		let dx = e.pageX - dropZone.offset().left;
+		let dy = e.pageY - dropZone.offset().top;
+		if ((dx < 0) || (dx > dropZone.width()) || (dy < 0) || (dy > dropZone.height())) {
+			dropZone.removeClass('dragover');
+		}
+	});
+
+	dropZone.on('drop', function(e) {
+		dropZone.removeClass('dragover');
+		let files = e.originalEvent.dataTransfer.files;
+		//sendFiles(files);
+        readURL(e.originalEvent.dataTransfer);
+	});
+
+	$('#file-input').change(function() {
+		let files = this.files;
+		//sendFiles(files);
+        readURL(this);
+	});
+
+    /*
+	function sendFiles(files) {
+		let maxFileSize = 5242880;
+		let Data = new FormData();
+		$(files).each(function(index, file) {
+			if ((file.size <= maxFileSize) && ((file.type == 'image/png') || (file.type == 'image/jpeg'))) {
+				Data.append('file_user_photo', file);
+			}
+		});
+
+		$.ajax({
+			url: dropZone.attr('action'),
+			type: dropZone.attr('method'),
+			data: Data,
+			contentType: false,
+			processData: false,
+			success: function(data) {
+				//alert ('Файлы были успешно загружены!');
+			}
+		});
+	}
+    */
+    
+    /*Для отображения загруженного фото*/
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+    
+            reader.onload = function (e) {
+                $('#upload-image').attr('src', e.target.result);
+            };
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
 });
