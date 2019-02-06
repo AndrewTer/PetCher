5fc725df4f938d7e52f5fb20705d68a91d8ff77a
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-02-07 00:12:07 +0300

    Add files via upload

---
+++ blablacat/403.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+    <title>403 Forbidden | BlaBlaCat</title> 
+</head>
+
+<div class="grid-container">
+    <div class="parent">
+        <h1 class="not-found-title">Ошибка 403</h1>
+        <div class="not-found">
+            <div class="not-found-image-forbidden">
+                <a href="index.php"><img src="images/forbidden.png" width="auto" height="250" /></a>
+            </div>   
+            <div class="not-found-info">
+                <p style="font: 19px sans-serif; color: #383838; text-align: center; font-weight: 600;">А ты неплох, Дорогой Пользователь...<br />Однако тебе сюда нельзя!</p>
+                <p style="font: 17px sans-serif; color: #383838;">Почему?</p>
+                <p style="font: 17px sans-serif; color: #383838;">Ответ прост - у тебя просто нет прав доступа </p>
+                <p style="font: 17px sans-serif; color: #383838;">Мы сожалеем, но ничего личного, надеемся ты поймёшь и не затаишь обиду на нас)</p>
+                <p style="font: 18px sans-serif; color: #383838;">Для продолжения работы с сайтом ты можешь перейти на <a id="main" href="index.php">Главную страницу сайта</a>, ну или кликнуть по картинке.</p>
+                <p style="font: 18px sans-serif; color: #383838;">Всего доброго!)</p>
+            </div>         
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+</html>
