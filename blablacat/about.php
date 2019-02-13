513ca7400eb309518d1eb63450d562f2ef8a1f4a
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-02-13 03:05:38 +0300

    Add files via upload

---
+++ blablacat/about.php
@@ -0,0 +1,55 @@
+<head>
+    <link href="css/style-login.css" media="screen" rel="stylesheet"/>
+    <!--<script type="text/javascript" src="/js/header.js"></script>
+    <script type="text/javascript" src="/js/jquery-1.7.1.min.js"></script>--!>
+    <title>О сервисе | BlaBlaCat</title>
+</head>
+<div class="header">
+    <div class="contain clearfix">
+        <a href=""><img id = "logos" src='images/logo.png' width="150" height="50" /></a>
+        <nav>
+            <a href="register.php">Регистрация</a>
+            <a href="">Правила</a>
+        </nav>
+    </div>
+</div>
+<div class="container_about about">
+    <p id="title-about">BlaBlaCat - сервис по передержке домашних животных</p>
+    <hr />
+    <p id="title-main-targer">Наша цель - помощь владельцам животных в поиске желающих посидеть с их питомцами на время отсутствия, создавая для этого удобные средства поиска и связи.</p>
+    <hr />
+    <p id="title-main-year-start">2019 год - является годом запуска сервиса BlaBlaCat !</p>
+    
+    <div class="div-for-map">
+        <p id="text-for-map">BlaBlaCat предоставляет возможность поиска пользователей сервиса в 20 городах России</p>
+        <img id="map_about" width="100%" src="images/map_about.png"/>
+        <span class="metka moscow" title="Москва"></span>
+        <span class="metka st_petersburg" title="Санкт-Петербург"></span>
+        <span class="metka volgograd" title="Волгоград"></span>
+        <span class="metka vladivostok" title="Владивосток"></span>
+        <span class="metka voronezh" title="Воронеж"></span>
+        <span class="metka yekaterinburg" title="Екатеринбург"></span>
+        <span class="metka kazan" title="Казань"></span>
+        <span class="metka kaliningrad" title="Калининград"></span>
+        <span class="metka krasnodar" title="Краснодар"></span>
+        <span class="metka krasnoyarsk" title="Красноярск"></span>
+        <span class="metka nizhny_novgorod" title="Нижний Новгород"></span>
+        <span class="metka novosibirsk" title="Новосибирск"></span>
+        <span class="metka omsk" title="Омск"></span>
+        <span class="metka permian" title="Пермь"></span>
+        <span class="metka rostov_on_don" title="Ростов-на-Дону"></span>
+        <span class="metka samara" title="Самара"></span>
+        <span class="metka ufa" title="Уфа"></span>
+        <span class="metka chelyabinsk" title="Челябинск"></span>
+        <span class="metka sevastopol" title="Севастополь"></span>
+        <span class="metka simferopol" title="Симферополь"></span>
+    </div>
+    <p id="text-for-map-add">Остальные города России также могут быть указаны пользователями сервиса в описании.</p>
+    <hr />
+    
+    <h3>Для связи с нами</h3>
+    <hr />
+</div>
+<div class="footer">
+BlaBlaCat © 2019
+</div>
