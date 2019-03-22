cc3240477bd0f00ec34d5a50e0c17338a37db653
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-03-23 01:02:04 +0300

    Add files via upload

---
+++ petcher/includes/support_body.php
@@ -0,0 +1,76 @@
+<div class="container_support support">
+    <p id="title-support">Раздел помощи</p>
+    <hr />
+    
+    <div class="block-show-current-support-my-page">
+        <p id="title-main-targer-support-up">Моя страница <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-my-page">
+        
+        
+    </div>
+    
+    <div class="block-show-current-support-settings">
+        <p id="title-main-targer-support-up">Настройки <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-settings">
+        Раздел <a id="text-support-part-link-to-page" href="settings.php">«Настройки»</a> находится в верхнем меню вашей страницы.
+        <img width="700px" class="support-image-settings" src="images/support/settings-menu-info.png" title="Раздел настроек в верхнем меню" />
+        В этом разделе вам доступно редактировать ваш номер мобильного телефона, адрес вашей электронной почты (логин) и ваш пароль от данного сервиса.
+        <img width="400px" class="support-image-settings" src="images/support/settings-page-info.png" title="Страница настроек" />
+    </div>
+    
+    <div class="block-show-current-support-orders">
+        <p id="title-main-targer-support-up">О разделе «Заказы» <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-orders">
+        asdasd2
+    </div>
+    
+    <div class="block-show-current-support-reviews">
+        <p id="title-main-targer-support-up">О разделе «Отзывы» <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-reviews">
+        asdasd3
+    </div>
+    
+    <div class="block-show-current-support-fav">
+        <p id="title-main-targer-support-up">О разделе «Избранное» <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-fav">
+        <img width="200px" class="support-image-settings-left" src="images/support/fav-menu-info.png" title="Раздел избранное в пользовательском меню" />
+        <p class="right-text-support-part">Раздел <a id="text-support-part-link-to-page" href="favourites.php">«Избранное»</a> находится в пользовательском меню вашей страницы, расположенном под вашей фотографией (изображение слева).</p>
+        <p class="right-text-support-part">Данный раздел нужен для отображения важных вам пользователей и быстрого перехода на их страницы путём клика по фотографиям.</p>
+        <p class="right-text-support-part"><img width="600px" class="support-image-settings" src="images/support/fav-page-info.png" title="Страница с разделом избранного" /></p>
+        <div class="clear"></div>
+    </div>
+    
+    <div class="block-show-current-support-req">
+        <p id="title-main-targer-support-up">О разделе «Заявки от ситтеров» <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-req">
+        asdasd 5
+    </div>
+    
+    <div class="block-show-current-support-resp">
+        <p id="title-main-targer-support-up">О разделе «Ответы на мои заявки» <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-resp">
+        asdasd 6
+    </div>
+    
+    <div class="block-show-current-support-search-users">
+        <p id="title-main-targer-support-up">Поиск пользователей <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-search-users">
+        asdasd 7
+    </div>
+    
+    <div class="block-show-current-support-search-orders">
+        <p id="title-main-targer-support-up">Поиск заказов <sub>﹀</sub></p>
+    </div>
+    <div class="block-support-info-search-orders">
+        asdasd 8
+    </div>
+
+</div>
