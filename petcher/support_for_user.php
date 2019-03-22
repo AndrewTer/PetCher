eeea75412a152b0253b28a8a0e1c3484df65a6da
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-03-23 01:04:44 +0300

    Add files via upload

--- petcher/support_for_user.php
+++ petcher/support_for_user.php
@@ -1,64 +1,78 @@
+<?php 
+require_once("includes/connection.php"); 
+include("functions.php");
+session_start();
+if($_SESSION['auth_user'] == "yes_auth")
+{
+    $username = $_SESSION['username'];
+    $encrypted_password = $_SESSION['encrypted_password'];
+    $email = $_SESSION['email'];
+    $phone = $_SESSION['phone'];
+    $street = $_SESSION['street'];
+    $id = $_SESSION['id']; 
+?>
+
+<html>
 <head>
-    <link href="css/style-login.css" media="screen" rel="stylesheet"/>
+    <link href="css/main.css" media="screen" rel="stylesheet"/>
     <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
-    <script type="text/javascript" src="js/share.js"></script>
+    <script type="text/javascript" src="js/script.js"></script> 
     <title>Помощь | PetCher</title>
 </head>
-<div class="header">
-    <div class="contain clearfix">
-        <a href=""><img id = "logos" src='images/logo.png' width="150" height="50" /></a>
-        <nav>
-            <a href="login.php">Вход</a>
-            <a href="register.php">Регистрация</a>
-            <a href="">Правила</a>
-            <a href="support_for_user.php">Помощь</a>
-            <a href="about.php">О нас</a>
-        </nav>
+<?php 
+    include("includes/header.php");
+?>
+
+<div class="grid-container">
+<?php 
+    include("includes/header.php");
+?>
+  
+<div class="body">
+    <div class="main-body">
+    <?php 
+        include("includes/support_body.php");
+    ?>    
     </div>
 </div>
-<div class="container_about about">
-    <p id="title-about">Раздел помощи</p>
-    <hr />
-    <p id="title-main-targer-support">Как создать свою страницу <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Как добавить информацию о себе <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Как добавить питомца <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Всё о заказах <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Всё о пользователях <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Всё об ответах <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Всё о заявках <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Всё об отзывах <sub>﹀</sub></p>
-    <p id="title-main-targer-support">Всё о настройках <sub>﹀</sub></p>
-
-</div>
-
+  
 <script>
-    $(document).ready(function() {
-        $('body').append('<div class="button-up" style="display: none; margin-top:50px; opacity: 0.7;width: 100%;max-width:90px;height:100%;position: fixed;left: 0px;top: 0px;cursor: pointer;text-align: center;line-height: 100px;color: #45688E;">&#9650; Наверх</div>');
-        $ (window).scroll (function () {
-            if ($ (this).scrollTop () > 300) {
-            $ ('.button-up').fadeIn();
-            } else {
-            $ ('.button-up').fadeOut();
-            }
-        });
-        $('.button-up').click(function(){
-            $('body,html').animate({
-                scrollTop: 0
-            }, 100);
-            return false;
-        });
-        $('.button-up').hover(function() {
-            $(this).animate({
-                'opacity':'1',
-            }).css({'background-color':'#E1E7ED','color':'#45688E'});
-        }, function(){
-            $(this).animate({
-                'opacity':'0.7'
-            }).css({'background':'none','color':'#45688E'});;
-        });
+$(document).ready(function() {
+    $('body').append('<div class="button-up" style="display: none; margin-top:50px; opacity: 0.7;width: 100%;max-width:90px;height:100%;position: fixed;left: 0px;top: 0px;cursor: pointer;text-align: center;line-height: 100px;color: #45688E;">&#9650; Наверх</div>');
+    $ (window).scroll (function () {
+        if ($ (this).scrollTop () > 300) {
+        $ ('.button-up').fadeIn();
+        } else {
+        $ ('.button-up').fadeOut();
+        }
     });
+    $('.button-up').click(function(){
+        $('body,html').animate({
+            scrollTop: 0
+        }, 100);
+        return false;
+    });
+    $('.button-up').hover(function() {
+        $(this).animate({
+            'opacity':'1',
+        }).css({'background-color':'#E1E7ED','color':'#45688E'});
+    }, function(){
+        $(this).animate({
+            'opacity':'0.7'
+        }).css({'background':'none','color':'#45688E'});;
+    });
+});
 </script>
-
-<div class="footer">
-PetCher © 2019
+  
+<?php 
+    include("includes/footer.php");
+?> 
+  
 </div>
+</html>
+<?
+}else
+{
+    header("Location: login.php"); 
+}
+?>
